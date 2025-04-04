e4edb7002faa75f14b3325b928626bd1c7d2b99b
Aliaksandr Shabanovich <alikshabanovich@gmail.com>
2025-04-04 05:03:21 +0300

    feat: add calculator

--- src/index.js
+++ src/index.js
@@ -1,1 +1,143 @@
-console.log("hello");
+import "./styles/main.css";
+const buttonValues = [
+  "AC",
+  "+/-",
+  "%",
+  "÷",
+  "7",
+  "8",
+  "9",
+  "×",
+  "4",
+  "5",
+  "6",
+  "-",
+  "1",
+  "2",
+  "3",
+  "+",
+  "0",
+  ".",
+  "=",
+];
+const buttonsOnTheRight = ["÷", "×", "-", "+", "="];
+const buttonsOnTheTop = ["AC", "+/-", "%"];
+let a = 0;
+let b = null;
+let operator = null;
+const display = document.querySelector(".display");
+display.value = "0";
+
+function clearAll() {
+  a = null;
+  b = null;
+  operator = null;
+}
+
+for (const buttonValue of buttonValues) {
+  let button = document.createElement("button");
+  button.innerText = buttonValue;
+  if (buttonsOnTheRight.includes(buttonValue)) {
+    button.style.backgroundColor = "#ff9f0a";
+  }
+  if (buttonsOnTheTop.includes(buttonValue)) {
+    button.style.backgroundColor = "#535353";
+  }
+  if (buttonValue === "0") {
+    button.style.width = "150px";
+    button.style.gridColumn = "span 2";
+  }
+
+  button.addEventListener("click", () => {
+    if (buttonsOnTheRight.includes(buttonValue)) {
+      if (buttonValue === "=") {
+        if (a !== null) {
+          b = display.value;
+          let numA = Number(a);
+          let numB = Number(b);
+
+          if (operator === "÷") {
+            display.value = numA / numB;
+          } else if (operator === "×") {
+            display.value = numA * numB;
+          } else if (operator === "-") {
+            display.value = numA - numB;
+          } else if (operator === "+") {
+            display.value = numA + numB;
+          }
+          clearAll();
+        }
+      } else {
+        operator = buttonValue;
+        a = display.value;
+        display.value = "";
+      }
+    } else if (buttonsOnTheTop.includes(buttonValue)) {
+      if (buttonValue === "AC") {
+        clearAll();
+        display.value = 0;
+      } else if (buttonValue === "+/-") {
+        if (display.value != "" && display.value != "0") {
+          if (display.value[0] == "-") {
+            display.value = display.value.slice(1);
+          } else {
+            display.value = "-" + display.value;
+          }
+        }
+      } else if (buttonValue === "%") {
+        display.value = Number(display.value) / 100;
+      }
+    } else {
+      if (buttonValue === ".") {
+        if (display.value != "" && !display.value.includes(buttonValue)) {
+          display.value += buttonValue;
+        }
+      } else if (display.value === "0") {
+        display.value = buttonValue;
+      } else {
+        display.value += buttonValue;
+      }
+    }
+  });
+  document.querySelector(".buttons").appendChild(button);
+}
+
+document.addEventListener("keydown", function (event) {
+  let keyValue = event.key;
+  const keys = ["Enter", "*", "/"];
+  if (buttonValues.includes(keyValue) || keys.includes(keyValue)) {
+    if (buttonsOnTheRight.includes(keyValue) || keys.includes(keyValue)) {
+      if (keyValue === "=" || keyValue === "Enter") {
+        console.log(a, b);
+        if (a !== null) {
+          b = display.value;
+          let numA = Number(a);
+          let numB = Number(b);
+
+          if (operator === "/") {
+            display.value = numA / numB;
+          } else if (operator === "*") {
+            display.value = numA * numB;
+          } else if (operator === "-") {
+            display.value = numA - numB;
+          } else if (operator === "+") {
+            display.value = numA + numB;
+          }
+          clearAll();
+        }
+      } else {
+        operator = keyValue;
+        a = display.value;
+        display.value = "";
+      }
+    } else if (keyValue === ".") {
+      if (display.value != "" && !display.value.includes(keyValue)) {
+        display.value += keyValue;
+      }
+    } else if (display.value === "0") {
+      display.value = keyValue;
+    } else {
+      display.value += keyValue;
+    }
+  }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,49 @@
-import { resolve } from "path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import ESLintPlugin from "eslint-webpack-plugin";
 import path from "path";
 import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-export const mode = "development";
-export const entry = resolve(__dirname, "src/index.js");
-export const output = {
-  path: resolve(__dirname, "dist"),
-  filename: "[name][contenthash].js",
-  clean: true,
-};
-export const devtool = "source-map";
-export const devServer = {
-  static: {
-    directory: resolve(__dirname, "dist"),
+const module = {
+  mode: "production",
+  entry: path.resolve(__dirname, "src/index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name][contenthash].js",
+    clean: true,
   },
-  port: 3000,
-  open: true,
-  hot: true,
-  compress: true,
-  historyApiFallback: true,
-};
-export const module = {
-  rules: [
-    { test: /\.css$/, use: "css-loader" },
-    {
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: "babel-loader",
-        options: { presets: ["@babel/preset-env"] },
-      },
+  devtool: "source-map",
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
     },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      { test: /\.css$/i, use: ["style-loader", "css-loader"] },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: { presets: ["@babel/preset-env"] },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Calculator",
+      filename: "index.html",
+      template: "./src/index.html",
+    }),
+    new ESLintPlugin(),
   ],
 };
-export const plugins = [
-  new HtmlWebpackPlugin({
-    title: "Calculator",
-    filename: "index.html",
-    template: "./src/index.html",
-  }),
-  new ESLintPlugin(),
-];
+
+export default module;
